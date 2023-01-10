8ac93ea953e6890c3ace520aab89abbd32279f7e
manuel peña <manpenap@gmail.com>
2023-01-10 18:17:26 -0300

    Updates

---
+++ assets/main.js
@@ -0,0 +1,43 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC7G1eUOxUw-uIccQ9ft2-Yg&part=snippet%2Cid&order=date&maxResults=6';
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c00bd4e1d4msh923a176a41ca6f1p189b95jsn7c980ca1f385',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi){
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async ()=>{
+    try {
+       const videos = await fetchData(API); 
+       console.log(videos);
+       let view = `
+       ${videos.items.map(video=>`
+        <div class="group relative">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${video.snippet.title}
+                </h3>
+            </div>
+        </div>
+       `).slice(0,4).join('')}
+
+       `;
+       content.innerHTML = view;
+    } catch (error) {
+        console.log(error);
+    }
+})();
